86e9a001f2b5c5ced8072c69731255206ed4e238
elpulpo0 <chris.elpulpo@gmail.com>
2024-11-06 06:26:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+```
+python -m venv venv
+```
+
+
+```
+source venv/Scripts/activate
+```
+
+
+```
+pip install --upgrade pip
+pip install -r requirements_local.txt
+```
+
+```
+python scrap.py
+```
+
+
---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+# Load environment variables
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Connect to MongoDB
+try:
+    client = MongoClient(MONGO_URI)
+    db = client['herotag']
+    collection = db['herotag_list']
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+    exit()
+
+@app.route("/get_herotags", methods=["GET", "POST"])
+def get_herotags():
+    if request.method == "GET":
+        # Get addresses from URL parameters
+        addresses = request.args.get("addresses")
+        if addresses:
+            addresses = addresses.split(",")  # Split the string into a list
+        else:
+            return jsonify({"error": "No addresses provided in the URL."}), 400
+
+        # MongoDB query to find herotags by address
+        query = {"address": {"$in": addresses}}
+        results = collection.find(query, {"address": 1, "userName": 1})
+        
+        # Create response with address and herotag
+        response_data = [{"address": result["address"], "herotag": result["userName"]} for result in results]
+
+        return jsonify(response_data)
+
+    elif request.method == "POST":
+        data = request.get_json()
+        addresses = data.get("addresses", [])
+        
+        if not addresses:
+            return jsonify({"error": "The list of addresses is empty or missing."}), 400
+
+        # MongoDB query to find herotags by address
+        query = {"address": {"$in": addresses}}
+        results = collection.find(query, {"address": 1, "userName": 1})
+        
+        # Create response with address and herotag
+        response_data = [{"address": result["address"], "herotag": result["userName"]} for result in results]
+
+        return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=False)
